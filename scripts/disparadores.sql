b3478c891e44060eaed5890ea9f37f38b23e27ad
angeel22m <mmoises.martinez0139@gmail.com>
2024-12-04 18:19:41 -0600

    modified:   scripts/disparadores.sql

--- scripts/disparadores.sql
+++ scripts/disparadores.sql
@@ -1,5 +1,6 @@
+ compania;
 -- tablas para llevar registro de la bitacora plantas
-CREATE TABLE Bitacora_Plantas (
+CREATE TABLE BITACORA_PLANTAS (
     idLog INT AUTO_INCREMENT PRIMARY KEY,
     idPlanta INT NOT NULL,
     accion ENUM('INSERT', 'UPDATE', 'DELETE') NOT NULL,
@@ -10,11 +11,11 @@ CREATE TABLE Bitacora_Plantas (
 -- Disparador para cuando se inserte una planta
 DELIMITER $$
 
-CREATE TRIGGER after_insert_plantas 
-AFTER INSERT ON Plantas
+CREATE TRIGGER AFTER_INSERT_PLANTAS 
+AFTER INSERT ON PLANTAS
 FOR EACH ROW
 BEGIN
-    INSERT INTO Bitacora_Plantas (idPlanta, accion, descripcion)
+    INSERT INTO BITACORA_PLANTAS (idPlanta, accion, descripcion)
     VALUES (NEW.idPlanta, 'INSERT', CONCAT('Se creó la planta con ID: ', NEW.idPlanta));
 END$$
 
@@ -23,11 +24,11 @@ DELIMITER ;
 --Disparador para cuando se actualice una planta
 DELIMITER $$
 
-CREATE TRIGGER after_update_plantas
-AFTER UPDATE ON Plantas
+CREATE TRIGGER AFTER_UPDATE_PLANTAS
+AFTER UPDATE ON PLANTAS
 FOR EACH ROW
 BEGIN
-    INSERT INTO Bitacora_Plantas (idPlanta, accion, descripcion)
+    INSERT INTO BITACORA_PLANTAS (idPlanta, accion, descripcion)
     VALUES (OLD.idPlanta, 'UPDATE', CONCAT('Se actualizo la planta con ID: ', OLD.idPlanta));
 END$$
 
@@ -38,18 +39,18 @@ DELIMITER ;
 DELIMITER $$
 
 CREATE TRIGGER after_delete_plantas
-AFTER DELETE ON Plantas
+AFTER DELETE ON PLANTAS
 FOR EACH ROW
 BEGIN
-    INSERT INTO Bitacora_Plantas (idPlanta, accion, descripcion)
+    INSERT INTO BITACORA_PLANTAS (idPlanta, accion, descripcion)
     VALUES (OLD.idPlanta, 'DELETE', CONCAT('Se elimino la planta con ID: ', OLD.idPlanta));
 END$$
 
 DELIMITER ;
 
 
--- tabla para llevar registro de la bitacora clientes
-CREATE TABLE bitacora_clientes (
+-- tabla para llevar registro de la bitacora CLIENTES
+CREATE TABLE BITACORA_CLIENTES (
     idLog INT AUTO_INCREMENT PRIMARY KEY,
     idCliente INT NOT NULL,
     accion ENUM('INSERT', 'UPDATE', 'DELETE') NOT NULL,
@@ -60,11 +61,11 @@ CREATE TABLE bitacora_clientes (
 -- Disparador para cuando se inserte un cliente
 DELIMITER $$
 
-CREATE TRIGGER after_insert_clientes 
-AFTER INSERT ON clientes
+CREATE TRIGGER AFTER_INSERT_CLIENTES 
+AFTER INSERT ON CLIENTES
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_clientes(idCliente, accion, descripcion)
+    INSERT INTO BITACORA_CLIENTES(idCliente, accion, descripcion)
     VALUES (NEW.idCliente, 'INSERT', CONCAT('Se creó la planta con ID: ', NEW.idCliente));
 END$$
 
@@ -73,11 +74,11 @@ DELIMITER ;
 -- Disparador para cuando se actualice un cliente
 DELIMITER $$
 
-CREATE TRIGGER after_update_clientes
-AFTER UPDATE ON clientes
+CREATE TRIGGER AFTER_UPDATE_CLIENTES
+AFTER UPDATE ON CLIENTES
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_clientes (idCliente, accion, descripcion)
+    INSERT INTO BITACORA_CLIENTES (idCliente, accion, descripcion)
     VALUES (OLD.idCliente, 'UPDATE', CONCAT('Se actualizo el cliente con ID: ', OLD.idCliente));
 END$$
 
@@ -86,18 +87,18 @@ DELIMITER ;
 -- Disparador para cuando se elimine un cliente
 DELIMITER $$
 
-CREATE TRIGGER after_delete_clientes
-AFTER DELETE ON clientes
+CREATE TRIGGER AFTER_DELETE_CLIENTES
+AFTER DELETE ON CLIENTES
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_clientes (idCliente, accion, descripcion)
+    INSERT INTO BITACORA_CLIENTES (idCliente, accion, descripcion)
     VALUES (OLD.idCliente, 'DELETE', CONCAT('Se elimino el cliente con ID: ', OLD.idCliente));
 END$$
 
 DELIMITER ; 
 
--- tabla para llevar registro de la bitacora concesionarios
-CREATE TABLE bitacora_concesionarios (
+-- tabla para llevar registro de la bitacora CONCESIONARIOS
+CREATE TABLE BITACORA_CONCESIONARIOS (
     idLog INT AUTO_INCREMENT PRIMARY KEY,
     idConcesionario INT NOT NULL,
     accion ENUM('INSERT', 'UPDATE', 'DELETE') NOT NULL,
@@ -108,11 +109,11 @@ CREATE TABLE bitacora_concesionarios (
 -- Disparador para cuando se inserte un concesionario
 DELIMITER $$
 
-CREATE TRIGGER after_insert_concesionario 
-AFTER INSERT ON concesionarios
+CREATE TRIGGER AFTER_INSERT_CONCESIONARIOS 
+AFTER INSERT ON CONCESIONARIOS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_concesionarios(idConcesionario, accion, descripcion)
+    INSERT INTO BITACORA_CONCESIONARIOS(idConcesionario, accion, descripcion)
     VALUES (NEW.idConcesionario, 'INSERT', CONCAT('Se creó el concesionario con ID: ', NEW.idConcesionario));
 END$$
 
@@ -121,11 +122,11 @@ DELIMITER ;
 -- Disparador para cuando se actualice un concensionario
 DELIMITER $$
 
-CREATE TRIGGER after_update_concensionarios
-AFTER UPDATE ON concesionarios
+CREATE TRIGGER AFTER_UPDATE_CONCESIONARIOS
+AFTER UPDATE ON CONCESIONARIOS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_concesionarios (idConcesionario, accion, descripcion)
+    INSERT INTO BITACORA_CONCESIONARIOS (idConcesionario, accion, descripcion)
     VALUES (OLD.idConcesionario, 'UPDATE', CONCAT('Se actualizo el concensionario con ID: ', OLD.idConcesionario));
 END$$
 
@@ -134,18 +135,18 @@ DELIMITER ;
 -- Disparador para cuando se elimine un concensionario
 DELIMITER $$
 
-CREATE TRIGGER after_delete_concensionarios
-AFTER DELETE ON concesionarios
+CREATE TRIGGER AFTER_DELETE_CONCESIONARIOS
+AFTER DELETE ON CONCESIONARIOS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_concesionarios (idConcesionario, accion, descripcion)
+    INSERT INTO BITACORA_CONCESIONARIOS (idConcesionario, accion, descripcion)
     VALUES (OLD.idConcesionario, 'DELETE', CONCAT('Se elimino el concensionario con ID: ', OLD.idConcesionario));
 END$$
 
 DELIMITER ; 
 
--- tabla para llevar registro de la bitacora modelos
-CREATE TABLE bitacora_modelos (
+-- tabla para llevar registro de la bitacora MODELOS
+CREATE TABLE BITACORA_MODELOS (
     idLog INT AUTO_INCREMENT PRIMARY KEY,
     idModelo INT NOT NULL,
     accion ENUM('INSERT', 'UPDATE', 'DELETE') NOT NULL,
@@ -156,11 +157,11 @@ CREATE TABLE bitacora_modelos (
 -- Disparador para cuando se inserte un modelo
 DELIMITER $$
 
-CREATE TRIGGER after_insert_modelos
-AFTER INSERT ON modelos
+CREATE TRIGGER AFTER_INSERT_MODELOS
+AFTER INSERT ON MODELOS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_modelos(idModelo, accion, descripcion)
+    INSERT INTO BITACORA_MODELOS(idModelo, accion, descripcion)
     VALUES (NEW.idModelo, 'INSERT', CONCAT('Se creó el modelo con ID: ', NEW.idModelo));
 END$$
 
@@ -169,11 +170,11 @@ DELIMITER ;
 -- Disparador para cuando se actualice un modelo
 DELIMITER $$
 
-CREATE TRIGGER after_update_modelos
-AFTER UPDATE ON modelos
+CREATE TRIGGER AFTER_UPDATE_MODELOS
+AFTER UPDATE ON MODELOS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_modelos (idModelo, accion, descripcion)
+    INSERT INTO BITACORA_MODELOS (idModelo, accion, descripcion)
     VALUES (OLD.idModelo, 'UPDATE', CONCAT('Se modifico el modelo con ID: ', OLD.idModelo));
 END$$
 
@@ -182,18 +183,18 @@ DELIMITER ;
 -- Disparador para cuando se elimine un modelo
 DELIMITER $$
 
-CREATE TRIGGER after_delete_modelos
-AFTER DELETE ON modelos
+CREATE TRIGGER AFTER_DELETE_MODELOS
+AFTER DELETE ON MODELOS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_modelos (idModelo, accion, descripcion)
+    INSERT INTO BITACORA_MODELOS (idModelo, accion, descripcion)
     VALUES (OLD.idModelo, 'DELETE', CONCAT('Se elimino el modelo con ID: ', OLD.idModelo));
 END$$
 
 DELIMITER ; 
 
--- tabla para llevar registro de la bitacora proveedores
-CREATE TABLE bitacora_proveedores(
+-- tabla para llevar registro de la bitacora PROVEEDORES
+CREATE TABLE BITACORA_PROVEEDORES(
     idLog INT AUTO_INCREMENT PRIMARY KEY,
     idProveedor INT NOT NULL,
     accion ENUM('INSERT', 'UPDATE', 'DELETE') NOT NULL,
@@ -204,11 +205,11 @@ CREATE TABLE bitacora_proveedores(
 -- Disparador para cuando se inserte un proveedor
 DELIMITER $$
 
-CREATE TRIGGER after_insert_proveedores
-AFTER INSERT ON proveedores
+CREATE TRIGGER AFTER_INSERT_PROVEEDORES
+AFTER INSERT ON PROVEEDORES
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_proveedores(idProveedor, accion, descripcion)
+    INSERT INTO BITACORA_PROVEEDORES(idProveedor, accion, descripcion)
     VALUES (NEW.idProveedor, 'INSERT', CONCAT('Se creó el proveedor con ID: ', NEW.idProveedor));
 END$$
 
@@ -217,11 +218,11 @@ DELIMITER ;
 -- Disparador para cuando se actualice un proveedor
 DELIMITER $$
 
-CREATE TRIGGER after_update_proveedores
-AFTER UPDATE ON proveedores
+CREATE TRIGGER AFTER_UPDATE_PROVEEDORES
+AFTER UPDATE ON PROVEEDORES
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_proveedores (idProveedor, accion, descripcion)
+    INSERT INTO BITACORA_PROVEEDORES (idProveedor, accion, descripcion)
     VALUES (OLD.idProveedor, 'UPDATE', CONCAT('Se modifico el proveedor con ID: ', OLD.idProveedor));
 END$$
 
@@ -230,18 +231,18 @@ DELIMITER ;
 -- Disparador para cuando se elimine un proveedor
 DELIMITER $$
 
-CREATE TRIGGER after_delete_proveedores
-AFTER DELETE ON proveedores
+CREATE TRIGGER AFTER_DELETE_PROVEEDORES
+AFTER DELETE ON PROVEEDORES
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_proveedores (idProveedor, accion, descripcion)
+    INSERT INTO BITACORA_PROVEEDORES (idProveedor, accion, descripcion)
     VALUES (OLD.idProveedor, 'DELETE', CONCAT('Se elimino el modelo con ID: ', OLD.idProveedor));
 END$$
 
 DELIMITER ; 
 
--- tabla para llevar registro de la bitacora vehiculos
-CREATE TABLE bitacora_vehiculos(
+-- tabla para llevar registro de la bitacora VEHICULOS
+CREATE TABLE BITACORA_VEHICULOS (
     idLog INT AUTO_INCREMENT PRIMARY KEY,
     VIN INT NOT NULL,
     accion ENUM('INSERT', 'UPDATE', 'DELETE') NOT NULL,
@@ -251,11 +252,11 @@ CREATE TABLE bitacora_vehiculos(
 -- Disparador para cuando se inserte un vehiculo
 DELIMITER $$
 
-CREATE TRIGGER after_insert_vehiculos
-AFTER INSERT ON vehiculos
+CREATE TRIGGER AFTER_INSERT_VEHICULOS
+AFTER INSERT ON VEHICULOS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_vehiculos(VIN, accion, descripcion)
+    INSERT INTO BITACORA_VEHICULOS(VIN, accion, descripcion)
     VALUES (NEW.VIN, 'INSERT', CONCAT('Se creó el vehiculo con VIN: ', NEW.VIN));
 END$$
 
@@ -264,11 +265,11 @@ DELIMITER ;
 -- Disparador para cuando se actualice un vehiculo
 DELIMITER $$
 
-CREATE TRIGGER after_update_vehiculos
-AFTER UPDATE ON vehiculos
+CREATE TRIGGER AFTER_UPDATE_VEHICULOS
+AFTER UPDATE ON VEHICULOS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_vehiculos (VIN, accion, descripcion)
+    INSERT INTO BITACORA_VEHICULOS (VIN, accion, descripcion)
     VALUES (OLD.VIN, 'UPDATE', CONCAT('Se modifico el vehiculo con VIN: ', OLD.VIN));
 END$$
 
@@ -277,18 +278,18 @@ DELIMITER ;
 -- Disparador para cuando se elimine un vehiculo
 DELIMITER $$
 
-CREATE TRIGGER after_delete_vehiculos
-AFTER DELETE ON vehiculos
+CREATE TRIGGER AFTER_DELETE_VEHICULOS
+AFTER DELETE ON VEHICULOS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_vehiculos (VIN, accion, descripcion)
+    INSERT INTO BITACORA_VEHICULOS (VIN, accion, descripcion)
     VALUES (OLD.VIN, 'DELETE', CONCAT('Se elimino el vehiculo con VIN: ', OLD.VIN));
 END$$
 
 DELIMITER ; 
 
--- tabla para llevar registro de la bitacora ventas
-CREATE TABLE bitacora_ventas(
+-- tabla para llevar registro de la bitacora VENTAS
+CREATE TABLE BITACORA_VENTAS(
     idLog INT AUTO_INCREMENT PRIMARY KEY,
     idVenta INT NOT NULL,
     accion ENUM('INSERT', 'UPDATE', 'DELETE') NOT NULL,
@@ -298,11 +299,11 @@ CREATE TABLE bitacora_ventas(
 -- Disparador para cuando se insertar una venta
 DELIMITER $$
 
-CREATE TRIGGER after_insert_ventas
-AFTER INSERT ON ventas
+CREATE TRIGGER AFTER_INSERT_VENTAS
+AFTER INSERT ON VENTAS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_ventas(idVenta, accion, descripcion)
+    INSERT INTO BITACORA_VENTAS(idVenta, accion, descripcion)
     VALUES (NEW.idVenta, 'INSERT', CONCAT('Se creó una venta con ID: ', NEW.idVenta));
 END$$
 
@@ -311,11 +312,11 @@ DELIMITER ;
 -- Disparador para cuando se actualice una venta
 DELIMITER $$
 
-CREATE TRIGGER after_update_ventas
-AFTER UPDATE ON ventas
+CREATE TRIGGER AFTER_UPDATE_VENTAS
+AFTER UPDATE ON VENTAS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_ventas (idVenta, accion, descripcion)
+    INSERT INTO BITACORA_VENTAS (idVenta, accion, descripcion)
     VALUES (OLD.idVenta, 'UPDATE', CONCAT('Se modifico la venta con ID : ', OLD.idVenta));
 END$$
 
@@ -324,12 +325,12 @@ DELIMITER ;
 -- Disparador para cuando se elimine un venta
 DELIMITER $$
 
-CREATE TRIGGER after_delete_ventas
-AFTER DELETE ON ventas
+CREATE TRIGGER AFTER_DELETE_VENTAS
+AFTER DELETE ON VENTAS
 FOR EACH ROW
 BEGIN
-    INSERT INTO bitacora_ventas (idVenta, accion, descripcion)
+    INSERT INTO BITACORA_VENTAS (idVenta, accion, descripcion)
     VALUES (OLD.idVenta, 'DELETE', CONCAT('Se elimino la venta con ID: ', OLD.idVenta));
 END$$
 
-DELIMITER ; 
+DELIMITER ;
