06e82478e63876ed1bc2860d1e0a009228e7909c
ShirleyR23 <shirleyhernandez197@gmail.com>
2024-12-05 19:57:00 -0600

    Update index.php

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -2,12 +2,18 @@
 // incluimos los controladores 
 require_once 'controllers/routes.controller.php';
 require_once 'controllers/clientes.controller.php';
+require_once 'controllers/plantas.controller.php';
+require_once 'controllers/modelos.controller.php';
+require_once 'controllers/proveedor.controller.php';
 require_once 'controllers/login.controller.php';
 require_once 'connection.php'; // Asegúrate de que la ruta sea correcta
 
 
 //incluimos los modelos
 require_once 'models/clientes.model.php';
+require_once 'models/modelos.model.php';
+require_once 'models/plantas.model.php';
+require_once 'models/proveedores.model.php';
 
 require_once './utf8_convert.php';
 
