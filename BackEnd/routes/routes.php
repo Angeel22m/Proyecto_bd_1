ac0f9f4fa9f213cc05d66f8a9628b322bc770313
ShirleyR23 <shirleyhernandez197@gmail.com>
2024-12-05 19:52:24 -0600

    Update routes.php

--- BackEnd/routes/routes.php
+++ BackEnd/routes/routes.php
@@ -27,6 +27,18 @@ if (count($arrayRutas) >= 3) {
             handleCliente($requestMethod);
             break;
 
+	case 'planta';
+            handlePlanta($requestMethod);
+            break;
+
+	case 'modelo';
+            handleModelo($requestMethod);
+            break;
+
+	case 'proveedor';
+            handleProveedor($requestMethod);
+            break;
+
         default:
             // Respuesta para rutas no encontradas
             echo json_encode([
@@ -59,7 +71,7 @@ function handleLogin($method) {
                 // Establece la conexión con el usuario correcto
                 $_SESSION['usuario'] = $nombreUsuario;
                 $_SESSION['password'] = $contrasena;
-                $_SESSION['rol'] = $rol;
+		$_SESSION['rol'] = $rol;
 
                 echo json_encode([
                     "status" => 200,
@@ -104,7 +116,11 @@ function handleCliente($method) {
                 $ingresosAnuales = $_POST['ingresosAnuales'];
 
                 try {
-                    $cliente->crearCliente($nombre, $direccion, $noTelefono, $sexo, $ingresosAnuales);                   
+                    $cliente->crearCliente($nombre, $direccion, $noTelefono, $sexo, $ingresosAnuales);
+                    echo json_encode([
+                        "status" => 201,
+                        "message" => "Cliente creado con éxito"
+                    ]);
                 } catch (Exception $e) {
                     echo json_encode([
                         "status" => 500,
@@ -131,49 +147,49 @@ function handleCliente($method) {
                 ]);
             }
             break;
-            case 'PUT':
-                // Obtener el idCliente de la ruta (en la URL)
-                $idCliente = isset($_GET['idCliente']) ? $_GET['idCliente'] : null;
-            
-                if (!$idCliente) {
-                    echo json_encode([
-                        "status" => 400,
-                        "error" => "El idCliente es obligatorio para actualizar un cliente."
-                    ]);
-                    break;
-                }
-            
-                // Leer el cuerpo de la solicitud para obtener los datos del cliente en form data
-                parse_str(file_get_contents("php://input"), $putData);
-            
-                // Validar que al menos un campo opcional esté presente
-                $campos = [];
-                if (!empty($putData['nombre'])) $campos['nombre'] = $putData['nombre'];
-                if (!empty($putData['direccion'])) $campos['direccion'] = $putData['direccion'];
-                if (!empty($putData['noTelefono'])) $campos['noTelefono'] = $putData['noTelefono'];
-                if (!empty($putData['sexo'])) $campos['sexo'] = $putData['sexo'];
-                if (!empty($putData['ingresosAnuales'])) $campos['ingresosAnuales'] = $putData['ingresosAnuales'];
-            
-                if (empty($campos)) {
-                    echo json_encode([
-                        "status" => 400,
-                        "error" => "Debe proporcionar al menos un campo para actualizar."
-                    ]);
-                    break;
-                }
-            
-                try {
-                    // Llamar al método para actualizar el cliente
-                    $cliente->actualizarCliente($idCliente, $campos);                               
-                } catch (Exception $e) {
-                    echo json_encode([
-                        "status" => 500,
-                        "error" => "Error interno al actualizar el cliente.",
-                        "detalle" => $e->getMessage()
-                    ]);
-                }
+
+        case 'PUT':
+            // Leer el cuerpo de la solicitud para datos JSON
+            parse_str(file_get_contents("php://input"), $putData);
+
+            if (!isset($putData['idCliente'])) {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "El idCliente es obligatorio para actualizar un cliente."
+                ]);
                 break;
-            
+            }
+
+            $idCliente = $putData['idCliente'];
+
+            // Validar que al menos un campo opcional esté presente
+            $campos = [];
+            if (!empty($putData['nombre'])) $campos['nombre'] = $putData['nombre'];
+            if (!empty($putData['direccion'])) $campos['direccion'] = $putData['direccion'];
+            if (!empty($putData['noTelefono'])) $campos['noTelefono'] = $putData['noTelefono'];
+            if (!empty($putData['sexo'])) $campos['sexo'] = $putData['sexo'];
+            if (!empty($putData['ingresosAnuales'])) $campos['ingresosAnuales'] = $putData['ingresosAnuales'];
+
+            if (empty($campos)) {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "Debe proporcionar al menos un campo para actualizar."
+                ]);
+                break;
+            }
+
+            try {
+                
+                $cliente->actualizarCliente($idCliente, $campos);               
+            } catch (Exception $e) {
+                echo json_encode([
+                    "status" => 500,
+                    "error" => "Error interno al actualizar el cliente.",
+                    "detalle" => $e->getMessage()
+                ]);
+            }
+            break;
+
             case 'DELETE':
                 // Obtener el ID del cliente desde los parámetros de la URL
                 if (isset($_GET['idCliente'])) {
@@ -206,6 +222,354 @@ function handleCliente($method) {
 }
 
 
+function handlePlanta($method) {
+    $planta = new PlantasController();
+
+    switch ($method) {
+        case 'POST':
+            // Verifica que los datos POST estén presentes
+            if (
+                isset($_POST['nombre'], $_POST['ubicacion'])
+            ) {
+                $nombre = $_POST['nombre'];
+                $ubicacion = $_POST['ubicacion'];
+
+                try {
+                    $planta->registrarNuevaPlanta($nombre, $ubicacion);
+                    echo json_encode([
+                        "status" => 201,
+                        "message" => "Planta creada con éxito"
+                    ]);
+                } catch (Exception $e) {
+                    echo json_encode([
+                        "status" => 500,
+                        "error" => "Error interno al crear la planta.",
+                        "detalle" => $e->getMessage()
+                    ]);
+                }
+            } else {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "Faltan datos obligatorios para crear la planta."
+                ]);
+            }
+            break;
+
+        case 'GET':
+            try {
+                $result = $planta->readAll();               
+            } catch (Exception $e) {
+                echo json_encode([
+                    "status" => 500,
+                    "error" => "Error interno al obtener las plantas.",
+                    "detalle" => $e->getMessage()
+                ]);
+            }
+            break;
+
+        case 'PUT':
+            // Leer el cuerpo de la solicitud para datos JSON
+            parse_str(file_get_contents("php://input"), $putData);
+
+            if (!isset($putData['idPlanta'])) {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "El idPlanta es obligatorio para actualizar una planta."
+                ]);
+                break;
+            }
+
+            $idPlanta = $putData['idPlanta'];
+
+            // Validar que al menos un campo opcional esté presente
+            $campos = [];
+            if (!empty($putData['nombre'])) $campos['nombre'] = $putData['nombre'];
+            if (!empty($putData['ubicacion'])) $campos['ubicacion'] = $putData['ubicacion'];
+
+            if (empty($campos)) {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "Debe proporcionar al menos un campo para actualizar."
+                ]);
+                break;
+            }
+
+            try {
+                
+                $planta->actualizarPlantaPorId($idPlanta, $campos);               
+            } catch (Exception $e) {
+                echo json_encode([
+                    "status" => 500,
+                    "error" => "Error interno al actualizar la planta.",
+                    "detalle" => $e->getMessage()
+                ]);
+            }
+            break;
+
+            case 'DELETE':
+                // Obtener el ID de la planta desde los parámetros de la URL
+                if (isset($_GET['idPlanta'])) {
+                    $idPlanta = $_GET['idPlanta'];
+    
+                    try {
+                        $planta->eliminarPlantaPorId($idPlanta);                       
+                    } catch (Exception $e) {
+                        echo json_encode([
+                            "status" => 500,
+                            "error" => "Error interno al eliminar la planta",
+                            "detalle" => $e->getMessage()
+                        ]);
+                    }
+                } else {
+                    echo json_encode([
+                        "status" => 400,
+                        "error" => "El idPlanta es obligatorio para eliminar una planta."
+                    ]);
+                }
+                break;
+
+        default:
+            echo json_encode([
+                "status" => 405,
+                "detalle" => "Método no permitido."
+            ]);
+            break;
+    }
+}
+
+function handleModelo($method) {
+    $modelo = new ModelosController ();
+
+    switch ($method) {
+        case 'POST':
+            // Verifica que los datos POST estén presentes
+            if (
+                isset($_POST['nombre'], $_POST['estiloCarroceria'], $_POST['marca'])
+            ) {
+                $nombre = $_POST['nombre'];
+                $estiloCarroceria = $_POST['estiloCarroceria'];
+                $marca = $_POST['marca'];
+
+                try {
+                    $modelo->nuevoModelo($nombre, $estiloCarroceria, $marca);
+                    echo json_encode([
+                        "status" => 201,
+                        "message" => "Modelo creado con éxito"
+                    ]);
+                } catch (Exception $e) {
+                    echo json_encode([
+                        "status" => 500,
+                        "error" => "Error interno al crear el modelo.",
+                        "detalle" => $e->getMessage()
+                    ]);
+                }
+            } else {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "Faltan datos obligatorios para crear el modelo."
+                ]);
+            }
+            break;
+
+        case 'GET':
+            try {
+                $result = $modelo->readAll();               
+            } catch (Exception $e) {
+                echo json_encode([
+                    "status" => 500,
+                    "error" => "Error interno al obtener los modelos.",
+                    "detalle" => $e->getMessage()
+                ]);
+            }
+            break;
+
+        case 'PUT':
+            // Leer el cuerpo de la solicitud para datos JSON
+            parse_str(file_get_contents("php://input"), $putData);
+
+            if (!isset($putData['idModelo'])) {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "El idModelo es obligatorio para actualizar un modelo."
+                ]);
+                break;
+            }
+
+            $idModelo = $putData['idModelo'];
+
+            // Validar que al menos un campo opcional esté presente
+            $campos = [];
+            if (!empty($putData['nombre'])) $campos['nombre'] = $putData['nombre'];
+            if (!empty($putData['estiloCarroceria'])) $campos['estiloCarroceria'] = $putData['estiloCarroceria'];
+            if (!empty($putData['marca'])) $campos['marca'] = $putData['marca'];
+            
+            if (empty($campos)) {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "Debe proporcionar al menos un campo para actualizar."
+                ]);
+                break;
+            }
+
+            try {
+                
+                $modelo->actualizarModelo($idModelo, $campos);               
+            } catch (Exception $e) {
+                echo json_encode([
+                    "status" => 500,
+                    "error" => "Error interno al actualizar el modelo.",
+                    "detalle" => $e->getMessage()
+                ]);
+            }
+            break;
+
+            case 'DELETE':
+                // Obtener el ID del modelo desde los parámetros de la URL
+                if (isset($_GET['idModelo'])) {
+                    $idModelo = $_GET['idModelo'];
+    
+                    try {
+                        $modelo->eliminarModelo($idModelo);                       
+                    } catch (Exception $e) {
+                        echo json_encode([
+                            "status" => 500,
+                            "error" => "Error interno al eliminar el modelo.",
+                            "detalle" => $e->getMessage()
+                        ]);
+                    }
+                } else {
+                    echo json_encode([
+                        "status" => 400,
+                        "error" => "El idModelo es obligatorio para eliminar un modelo."
+                    ]);
+                }
+                break;
+
+        default:
+            echo json_encode([
+                "status" => 405,
+                "detalle" => "Método no permitido."
+            ]);
+            break;
+    }
+}
+
+function handleProveedor($method) {
+    $proveedor = new ProveedoresController();
+
+    switch ($method) {
+        case 'POST':
+            // Verifica que los datos POST estén presentes
+            if (
+                isset($_POST['nombre'], $_POST['direccion'], $_POST['noTelefono'])
+            ) {
+                $nombre = $_POST['nombre'];
+                $direccion = $_POST['direccion'];
+                $noTelefono = $_POST['noTelefono'];
+
+                try {
+                    $proveedor->nuevoProveedor($nombre, $direccion, $noTelefono);
+                    echo json_encode([
+                        "status" => 201,
+                        "message" => "Proveedor creado con éxito"
+                    ]);
+                } catch (Exception $e) {
+                    echo json_encode([
+                        "status" => 500,
+                        "error" => "Error interno al crear el proveedor.",
+                        "detalle" => $e->getMessage()
+                    ]);
+                }
+            } else {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "Faltan datos obligatorios para crear el proveedor."
+                ]);
+            }
+            break;
+
+        case 'GET':
+            try {
+                $result = $proveedor->readAll();               
+            } catch (Exception $e) {
+                echo json_encode([
+                    "status" => 500,
+                    "error" => "Error interno al obtener los proveedores.",
+                    "detalle" => $e->getMessage()
+                ]);
+            }
+            break;
+
+        case 'PUT':
+            // Leer el cuerpo de la solicitud para datos JSON
+            parse_str(file_get_contents("php://input"), $putData);
+
+            if (!isset($putData['idProveedor'])) {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "El idProveedor es obligatorio para actualizar un proveedor."
+                ]);
+                break;
+            }
+
+            $idProveedor = $putData['idProveedor'];
+
+            // Validar que al menos un campo opcional esté presente
+            $campos = [];
+            if (!empty($putData['nombre'])) $campos['nombre'] = $putData['nombre'];
+            if (!empty($putData['direccion'])) $campos['direccion'] = $putData['direccion'];
+            if (!empty($putData['noTelefono'])) $campos['noTelefono'] = $putData['noTelefono'];
+
+            if (empty($campos)) {
+                echo json_encode([
+                    "status" => 400,
+                    "error" => "Debe proporcionar al menos un campo para actualizar."
+                ]);
+                break;
+            }
+
+            try {
+                
+                $proveedor->actualizarProveedor($idProveedor, $campos);               
+            } catch (Exception $e) {
+                echo json_encode([
+                    "status" => 500,
+                    "error" => "Error interno al actualizar el proveedor.",
+                    "detalle" => $e->getMessage()
+                ]);
+            }
+            break;
+
+            case 'DELETE':
+                // Obtener el ID del proveedor desde los parámetros de la URL
+                if (isset($_GET['idProveedor'])) {
+                    $idProveedor = $_GET['idProveedor'];
+    
+                    try {
+                        $proveedor->eliminarProveedor($idProveedor);                       
+                    } catch (Exception $e) {
+                        echo json_encode([
+                            "status" => 500,
+                            "error" => "Error interno al eliminar el proveedor.",
+                            "detalle" => $e->getMessage()
+                        ]);
+                    }
+                } else {
+                    echo json_encode([
+                        "status" => 400,
+                        "error" => "El idProveedor es obligatorio para eliminar un proveedor."
+                    ]);
+                }
+                break;
+
+        default:
+            echo json_encode([
+                "status" => 405,
+                "detalle" => "Método no permitido."
+            ]);
+            break;
+    }
+}
 
 
 
